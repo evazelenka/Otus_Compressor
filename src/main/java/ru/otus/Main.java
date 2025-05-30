49e05ff8767bcea41113fe745979f7ff92de84e2
zelenka <eva.zelya@gmail.com>
2025-05-30 23:33:24 +0300

    stats added

--- src/main/java/ru/otus/Main.java
+++ src/main/java/ru/otus/Main.java
@@ -2,6 +2,7 @@ package ru.otus;
 //Основы сжатия данных: создаем RLE архиватор(run length encoding, кодирование длин серий)
 //Демо-занятие курса "Алгоритмы и структуры данных"
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -23,10 +24,22 @@ public class Main {
 
     private static void compress(ICompress alg, String file, String fileZip) throws IOException {
         System.out.println("compress using " + alg.getName());
+        long fileLength = new File(file).length();
+        long startTime = System.currentTimeMillis();
         try(FileInputStream input = new FileInputStream(file);
             FileOutputStream output = new FileOutputStream(fileZip)){
             alg.compress(input, output);
         }
+
+        long finishTime = System.currentTimeMillis();
+        long fileZipLength = new File(fileZip).length();
+
+        double compressRatio = (double) fileZipLength / fileLength * 100;
+
+        System.out.println("Original size: " + fileLength);
+        System.out.println("Compress size: " + fileZipLength);
+        System.out.println(String.format("Ratio: %.2f%%%n", compressRatio));
+        System.out.println("Time: " + (finishTime - startTime) + " ms");
     }
 
     private static void decompress(ICompress alg, String fileZip, String fileUnzip) throws IOException {
